e24e59e052f6c42913ad0608e4f3c6317b953914
cokedispenser <ahnaftarik.official@gmail.com>
2026-05-03 04:56:46 +0600

    fixed bugs on memory allocation and added execution time and local time feature

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,23 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
+char *month[12] = {"jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"};
 int main(int argc , char *argv[])
 {
+    clock_t start = clock();
 
-    int max = 0 , min = 10000;
+    
     int *ptrA , sum = 0;
     int size = atoi(argv[1]);
-    if(argc != 2) printf("usage : .\\program.exe [array-size(int)]");
+    if(argc != 2) 
+    {
+        printf("usage : .\\program.exe [array-size(int)]");
+        return 1;
+    }
     ptrA = (int *)malloc(size * sizeof(int));
 
     for(int i = 0 ; i < size ; i++)
     {
         printf("number - %d : " , i + 1);
         scanf("%d" , ptrA + i);
-        sum += *(ptrA + i);
+       
     }
 
-    ptrA = (int *)realloc(ptrA , 2 * size);
+    ptrA = (int *)realloc(ptrA , 2 * size * sizeof(int));
 
     for(int i = size ; i < 2 * size ; i++)
     {
@@ -42,6 +49,26 @@ int main(int argc , char *argv[])
         }
         if(IsUnique) printf("%d " , *(ptrA + i));
     }
+
+    free(ptrA);
+    
+    clock_t end = clock();
+
+    float exec_time = (float)(end - start) / CLOCKS_PER_SEC;
+
+    time_t raw;
+
+    time(&raw);
+
+    struct tm *realtime;
+
+    realtime = localtime(&raw);
+
+    
+    printf("\n%d %s, %d\n" , realtime->tm_mday , month[realtime->tm_mon] , realtime->tm_year + 1900);
+    printf("execution time : %f" , exec_time);
+
+    return 0;
 
 }
 

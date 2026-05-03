8ebc9b981e01215721aa6d926f9dbd7ae8bbd6cd
cokedispenser <ahnaftarik.official@gmail.com>
2026-05-03 18:56:32 +0600

    updated the function pointer file to take multiple arguments

--- funcPtr.c
+++ funcPtr.c
@@ -1,32 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int add(int x , int y)
+int add(int *ptr, int size)
 {
-    return x + y;
-}
-int sub(int x , int y)
-{
-    return abs(x - y);
-}
-
-void calculate(int x , int y , int(*operation)(int , int))
-{
-    printf("result = %d" , operation(x , y));
-}
-
-int main(int argc , char *argv[])
-{
-
-    if(argc < 3) return 1;
-    else
+    int sum = 0;
+    int i = 0;
+    while(i < size)
     {
-        int x = atoi(argv[1]);
-        int y = atoi(argv[2]);
-        calculate(x , y , add);
-        calculate(x , y , sub);
+        sum += *(ptr + i);
+        i++;
+    }
+    return sum;
+}
+
+int sub(int *ptr, int size)
+{
+    int result = *(ptr + 0);
+    int i = 1;
+    while(i < size)
+    {
+        result -= *(ptr + i);
+        i++;
+    }
+    return result;
+}
+
+void calculate(int *arr, int size, int(*operation)(int *, int))
+{
+    printf("result = %d\n", operation(arr, size));
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc < 3) {
+        printf("usage: .\\program.exe [numbers...]\n");
+        return 1;
     }
 
+    int size = argc - 1;
+    int *arr = (int *)malloc(size * sizeof(int));
+    if(arr == NULL) {
+        printf("memory allocation failed\n");
+        return 1;
+    }
 
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = atoi(argv[i + 1]);
+    }
+
+    calculate(arr, size, add);
+    calculate(arr, size, sub);
+
+    free(arr);
     return 0;
 }

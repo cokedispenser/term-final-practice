b20944d6b74772e3a44690e72e3aa8f8d3bfe401
cokedispenser <ahnaftarik.official@gmail.com>
2026-05-03 18:38:46 +0600

    added  function pointers

---
+++ funcPtr.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int add(int x , int y)
+{
+    return x + y;
+}
+
+void calculate(int x , int y , int(*operation)(int , int))
+{
+    printf("result = %d" , operation(x , y));
+}
+
+int main(int argc , char *argv[])
+{
+
+    if(argc < 3) return 1;
+    else
+    {
+        int x = atoi(argv[1]);
+        int y = atoi(argv[2]);
+        calculate(x , y , add);
+    }
+
+
+    return 0;
+}
